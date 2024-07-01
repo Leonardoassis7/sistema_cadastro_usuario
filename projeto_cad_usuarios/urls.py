7293bd3814f2f0b381f4f9dfff55b62749b63f12
Leonardoassis7 <leonardorobertoassis@gmail.com>
2024-07-01 10:56:31 -0300

    Criação do banco de dados SQLite3 e funcionalidade do Django

--- projeto_cad_usuarios/urls.py
+++ projeto_cad_usuarios/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     # rota, view resposável, nome de referência
     #usuarios.com
     path('',views.home,name='home'),
+    #usuarios.com/usuarios
+    path('usuarios/',views.usuarios,name='listagem_usuarios')
 ]
